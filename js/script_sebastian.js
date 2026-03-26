ad5789a1b8fdfda0d8b6858091743869f44b8dbd
riggen212 <bastiriggen212@gmail.com>
2026-03-26 20:37:54 +0100

    input validation added

--- js/script_sebastian.js
+++ js/script_sebastian.js
@@ -1,16 +1,27 @@
 const TABLE = document.getElementById(`ingredients`);
-const SPANELEMENTLIST = TABLE.querySelectorAll(`span`)
-
+const SPANELEMENTLIST = TABLE.querySelectorAll(`span`);
+let errormessage = document.getElementById(`errorMessage`);
 let spanArray = [];
 
 function getSpanElements() {
     spanArray = Array.from(SPANELEMENTLIST).map(span => span.textContent.trim());
-} 
+}
 
-function calculateIngredients() {
-    const FACTOR = document.getElementById(`factor`).valueAsNumber;   
+function inputValidation() {
+    const FACTOR = document.getElementById(`factor`).valueAsNumber;
+
+    if (FACTOR < 1 || FACTOR > 15 || isNaN(FACTOR)) {
+        errormessage.textContent = "Dies ist keine gültige Eingabe. Bitte geben Sie eine Zahl zwischen 1 und 15 ein.";
+    }
+    else {
+        errormessage.textContent = "";
+        calculateIngredients(FACTOR);
+    }
+}
+
+function calculateIngredients(factor) {  
     for (let i = 0; i < spanArray.length; i++) {
-        let result = spanArray[i] * FACTOR;
+        let result = spanArray[i] * factor;
         SPANELEMENTLIST[i].textContent = result;       
     }    
 };
