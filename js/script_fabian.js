36ee120d04f6fc071a10242311d6823fcebf97b6
fabof1965 <fabian.gerdes1996@googlemail.com>
2026-04-17 15:16:41 +0200

    refactor: changed calculation logic and modified style.css

--- js/script_fabian.js
+++ js/script_fabian.js
@@ -1,37 +1,47 @@
-let inputField = document.getElementById("inputAmount");    
+let inputField = document.getElementById("inputAmount"); 
+const INGREDIENTS = document.getElementsByClassName("amountNumber");
+const calculationButton = document.getElementById("calculationButton");
+let error = document.getElementById("error");
 
-function determinePortion() {
-    const INGREDIENTS = document.getElementsByClassName("amountNumber");
-    inputValue = +inputField.value;
-    calculateIngredients(INGREDIENTS, inputValue);
-}
+let inputValue = 0;
+
+inputField.addEventListener("change", () => {
+    inputValue = inputField.value;
+    inputValidation();
+
+    if(inputValue <= 0 || inputValue > 10) {
+        setDefaultValues();
+    }
+}) 
 
 function inputValidation() {
     inputValue = +inputField.value;
-    let valueInvalid = inputValue <= 0 || inputValue > 10 || isNaN(inputValue) || inputValue == "";
-    let error = document.getElementById("error");
+    let valueInvalid = inputValue <= 0 || inputValue > 10;
 
     if (valueInvalid) {
         error.textContent = "Bitte geben Sie eine Zahl zwischen 1 und 10 ein. Die Rezeptangaben werden auf eine Menge von 4 Portionen gesetzt.";
-        inputField.classList.add("borderError");
-        setDefaultValues();
+        inputField.value = "";
+        
         return;
     }
-    
+
     error.textContent = "";
-    inputField.classList.remove("borderError");
+
 }
 
-function calculateIngredients(INGREDIENTS, inputValue) {
-    for (let i = 0; i < INGREDIENTS.length; i++) {
-        let newAmount = (INGREDIENTS[i].getAttribute("data-default") / 4) * inputValue;
-        INGREDIENTS[i].innerHTML = newAmount;
-    } 
+function calculateIngredients() {
+    inputValue = +inputField.value;
+
+    if(inputValue > 0 && inputValue <= 10) {
+        for (let i = 0; i < INGREDIENTS.length; i++) {
+            let newAmount = (INGREDIENTS[i].getAttribute("data-default") / 4) * inputValue;
+            INGREDIENTS[i].innerHTML = newAmount;
+        } 
+    }
 }
 
 function setDefaultValues() {
-    const INGREDIENTS = document.getElementsByClassName("amountNumber");
     for (let i = 0; i < INGREDIENTS.length; i++) {
         INGREDIENTS[i].innerHTML = INGREDIENTS[i].getAttribute("data-default");
-    }
+    }   
 }
